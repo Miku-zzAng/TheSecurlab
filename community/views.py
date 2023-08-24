921d06905e4576026b4872597e068edd18307df4
KIM <hako5460@naver.com>
2023-08-24 15:54:02 +0900

    글 수정 구현 완료

--- community/views.py
+++ community/views.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-
+from community.forms import PostForm
 
 def board(request):
     receivePage = request.GET.get("page", "1")  # 페이지
@@ -23,10 +23,10 @@ def post_detail(request, post_id):
     print(idTargetPost)
     idTargetPost.viewNum += 1
     idTargetPost.save()
+    max_post_id = Post.objects.latest('id').id
 
-    if request.method == "POST":
+    if request.method == "POST": # 상세보기 속에서 댓글작성 POST 요청 받을시
         if request.user.is_authenticated:
-            addPostUserId = request.user.id
             comment_content = request.POST["comment"]
             addComment = Comment.objects.create(
                 content=comment_content,
@@ -38,15 +38,15 @@ def post_detail(request, post_id):
 
     context = {
         "post": idTargetPost,
+        'max_post_id': max_post_id
     }
     return render(request, "community/post_detail.html", context)
 
 
 def post_add(request):
     if request.user.is_authenticated:
-        addPostUserId = request.user.id
 
-        if request.method == "POST":
+        if request.method == "POST": # POST 글 작성을 완료할 경우
             addPostTitle = request.POST["title"]
             addPostContent = request.POST["content"]
 
@@ -54,9 +54,10 @@ def post_add(request):
                 title=addPostTitle, content=addPostContent, writer=request.user
             )
             return redirect(f"/community/board/{addPost.id}")
-    else:
+    else: # 상세보기에서 글 작성하기 버튼을 누를 경우
         return redirect(f"/users/login/")
-    return render(request, "community/post_add.html")
+    context = {"action": "add"}
+    return render(request, "community/post_add.html", context)
 
 
 @login_required(login_url="/users/login/")
@@ -65,22 +66,18 @@ def post_modify(request, post_id):
 
     if request.user != targetPost.writer:
         messages.error(request, "수정 권한이 없습니다.")
-        return redirect(f"board/{targetPost.post_id}")
+        return redirect(f"/community/board/{targetPost.id}")
 
-    if request.method == "POST":
+    if request.method == "POST": # POST 요청인경우, 즉 수정을 완료했을경우
         modifyForm = PostForm(request.POST, instance=targetPost)
 
-        if targetPost.is_valid():
-            targetPost = modifyForm.save(commit=False)
-            targetPost.modify_date = timezone.now()
-            targetPost.save()
+        targetPost = modifyForm.save(commit=False) # commit=False는 DB에 저장하지 않고 객체만 반환, But 밑코드 save()함수가 저장해줌 
+        targetPost.modify_date = timezone.now()
+        targetPost.save()
         return redirect(f"/community/board/{targetPost.id}")
 
     else:  # GET 요청인경우, 즉 수정하기 버튼을 눌렀을 경우
-        modifyForm = Post(
-            instance=targetPost
-        )  # instance 속성으로 수정 누를시 글 내용과 동일하게 채워놔야함. 수정할수 있게
-
-    context = {"form": modifyForm}
-
-    return render(request, "community/post_add.html", context)
+        modifyForm = PostForm(instance=targetPost)  # instance 속성으로 수정 누를시 글 내용과 동일하게 채워놔야함. 수정할수 있게
+        context = {"form": modifyForm, "action": "modify", "post": targetPost}
+        print(targetPost.title)
+        return render(request, "community/post_add.html", context)
